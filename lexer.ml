dd9aa023f2fb87cc0062d4605f62bc805c498328
Kevin <ksrof@proton.me>
2023-06-21 00:12:09 +0200

    wip: started working on the lexer, added fn to initialize lexer and read char

---
+++ lexer.ml
@@ -0,0 +1,20 @@
+type lexer = {
+  mutable input: string;
+  mutable position: int;
+  mutable read_pos: int;
+  mutable ch: char;
+}
+
+let read_char (l: lexer) =
+  let read =
+    if l.read_pos >= String.length l.input then
+      l.ch <- '\x00'
+    else
+      l.ch <- String.get l.input l.read_pos
+    in read;
+
+  l.position <- l.read_pos;
+  l.read_pos <- l.read_pos + 1;;
+
+let new_lexer (input: string) = 
+  read_char { input = input; position = 0; read_pos = 0; ch = '\x00' };;
