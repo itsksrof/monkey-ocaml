e1f0a787eefa71d5555cfed45cf68e16e2c463e6
Kevin <ksrof@proton.me>
2023-06-28 23:22:55 +0200

    test: added first test to lex an Assign token

--- lexer_test.ml
+++ lexer_test.ml
@@ -1,3 +1,11 @@
-open Token
+let test_assign =
+  let input : string = "=" in
+  let lexer = Lexer.new_lexer input in
+  let tok = Lexer.next_token lexer in 
+    if tok.kind != Token.Assign then
+      Printf.eprintf "[failed] test_assign -> wrong token kind\n";
+    if String.compare tok.literal "=" != 0 then
+      Printf.eprintf "[failed] test_assign -> wrong token literal\n";;
 
-let () = hello ()
+let () =
+  test_assign
