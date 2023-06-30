49de468d68e3d7ac8e2bb837fde30721a6cb76a0
Kevin <ksrof@proton.me>
2023-07-01 00:32:22 +0200

    feat: added test_lexer_rightparen

--- lexer_test.ml
+++ lexer_test.ml
@@ -43,9 +43,19 @@ let test_lexer_leftparen =
     if String.compare tok.literal "(" != 0 then
       Printf.eprintf "[failed] test_lexer_leftparen -> wrong token literal\n";;
 
+let test_lexer_rightparen =
+  let input : string = ")" in
+  let lexer = Lexer.new_lexer input in
+  let tok = Lexer.next_token lexer in
+    if tok.kind != Token.RightParen then
+      Printf.eprintf "[failed] test_lexer_rightparen -> wrong token kind\n";
+    if String.compare tok.literal ")" != 0 then
+      Printf.eprintf "[failed] test_lexer_rightparen -> wrong token literal\n";;
+
 let () =
   test_lexer_assign;
   test_lexer_plus;
   test_lexer_comma;
   test_lexer_semicolon;
-  test_lexer_leftparen
+  test_lexer_leftparen;
+  test_lexer_rightparen
