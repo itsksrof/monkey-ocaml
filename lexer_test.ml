0ba3d9c9ba8187b8a213e11facbfb7316b26ba8b
Kevin <ksrof@proton.me>
2023-07-01 00:06:16 +0200

    style: updated test_lexer_assign error messages

--- lexer_test.ml
+++ lexer_test.ml
@@ -3,9 +3,9 @@ let test_lexer_assign =
   let lexer = Lexer.new_lexer input in
   let tok = Lexer.next_token lexer in 
     if tok.kind != Token.Assign then
-      Printf.eprintf "[failed] test_assign -> wrong token kind\n";
+      Printf.eprintf "[failed] test_lexer_assign -> wrong token kind\n";
     if String.compare tok.literal "=" != 0 then
-      Printf.eprintf "[failed] test_assign -> wrong token literal\n";;
+      Printf.eprintf "[failed] test_lexer_assign -> wrong token literal\n";;
 
 let test_lexer_plus =
   let input : string = "+" in
