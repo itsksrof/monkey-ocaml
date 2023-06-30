f73a8f30c1a3d90526422e5b44f9220bcf3544a1
Kevin <ksrof@proton.me>
2023-07-01 00:05:34 +0200

    feat: added test_lexer_plus

--- lexer_test.ml
+++ lexer_test.ml
@@ -7,5 +7,15 @@ let test_lexer_assign =
     if String.compare tok.literal "=" != 0 then
       Printf.eprintf "[failed] test_assign -> wrong token literal\n";;
 
+let test_lexer_plus =
+  let input : string = "+" in
+  let lexer = Lexer.new_lexer input in
+  let tok = Lexer.next_token lexer in
+    if tok.kind != Token.Plus then
+      Printf.eprintf "[failed] test_lexer_plus -> wrong token kind\n";
+    if String.compare tok.literal "+" != 0 then
+      Printf.eprintf "[failed] test_lexer_plus -> wrong token literal \n";;
+
 let () =
-  test_lexer_assign
+  test_lexer_assign;
+  test_lexer_plus
