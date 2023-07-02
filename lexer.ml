3d9f2a55286162c3f5a4455f68ea9d4beba84f46
Kevin <ksrof@proton.me>
2023-07-02 02:50:21 +0200

    refactor: restructured read_char, using String.sub with correct len, restructured next_token

--- lexer.ml
+++ lexer.ml
@@ -6,15 +6,12 @@ type lexer = {
 }
 
 let read_char (l: lexer) =
-  let read =
-    if l.read_pos >= String.length l.input then
-      l.ch <- char_of_int 0
-    else
-      l.ch <- String.get l.input l.read_pos
-    in 
-      l.position <- l.read_pos;
-      l.read_pos <- l.read_pos + 1;
-      read;; 
+  if l.read_pos >= String.length l.input then
+    l.ch <- char_of_int 0
+  else
+    l.ch <- String.get l.input l.read_pos;
+  l.position <- l.read_pos;
+  l.read_pos <- l.read_pos + 1;;
 
 let new_lexer (input: string) : lexer =
   let l =
@@ -33,21 +30,23 @@ let is_digit (ch: char) : bool =
   '0' <= ch && ch <= '9';;
 
 let is_eof (ch: char) : bool =
-    char_of_int 0 = ch;;
+  char_of_int 0 = ch;;
 
 let read_identifier (l: lexer) : string =
   let position = l.position in
     while is_letter(l.ch) do
       read_char(l)
     done;
-    String.sub l.input position l.position;;
+    let len = l.position - position in
+    String.sub l.input position len;;
 
 let read_number (l: lexer) : string =
   let position = l.position in
     while is_digit(l.ch) do
       read_char(l)
     done;
-    String.sub l.input position l.position;;
+    let len = l.position - position in
+    String.sub l.input position len;;
 
 let skip_whitespace (l: lexer) =
   while l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' do
@@ -56,26 +55,23 @@ let skip_whitespace (l: lexer) =
 
 let next_token (l: lexer) : Token.token =
   skip_whitespace(l);
-  let match_token =
-    match l.ch with
-    | '=' -> new_token Token.Assign l.ch
-    | '+' -> new_token Token.Plus l.ch
-    | ',' -> new_token Token.Comma l.ch
-    | ';' -> new_token Token.Semicolon l.ch
-    | '(' -> new_token Token.LeftParen l.ch
-    | ')' -> new_token Token.RightParen l.ch
-    | '{' -> new_token Token.LeftBrace l.ch
-    | '}' -> new_token Token.RightBrace l.ch
-    | _ -> 
-      if is_letter(l.ch) then
-        let literal = read_identifier(l) in
-          {kind = Token.lookup_identifier(literal); literal = literal}
-      else if is_digit(l.ch) then
-        {kind = Token.Int; literal = read_number(l)}
-      else if is_eof(l.ch) then
-        new_token Token.Eof l.ch
-      else
-        new_token Token.Illegal l.ch
-  in 
-    read_char(l);
-    match_token;; 
+  let tok = match l.ch with
+  | '=' -> new_token Token.Assign l.ch
+  | '+' -> new_token Token.Plus l.ch
+  | ',' -> new_token Token.Comma l.ch
+  | ';' -> new_token Token.Semicolon l.ch
+  | '(' -> new_token Token.LeftParen l.ch
+  | ')' -> new_token Token.RightParen l.ch
+  | '{' -> new_token Token.LeftBrace l.ch
+  | '}' -> new_token Token.RightBrace l.ch
+  | _ ->
+    if is_letter l.ch then
+      let literal = read_identifier l in
+      {kind = Token.lookup_identifier literal; literal = literal}
+    else if is_digit l.ch then
+      {kind = Token.Int; literal = read_number l}
+    else if is_eof l.ch then
+      new_token Token.Eof l.ch
+    else
+      new_token Token.Illegal l.ch
+    in read_char l; tok;;
